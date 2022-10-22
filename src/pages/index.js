89f5da67a0c610f14add25ed8e95b5e2b41c8e85
sanggggg <jacob@vcnc.co.kr>
2022-10-22 17:37:13 +0900

    init

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <li>
+        <div>
+          <div>
+            <ul>
+              <li>
+                <li>asdf</li>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>
+      <div className="mock">Hello..?</div>
+    </div>
+  );
+}
